969e69744d05c037e9d90056be086098af978401
Kletzander <kletzander@cip.ifi.lmu.de>
2018-05-09 17:25:55 +0200

    One (important) question about the last element

--- assignment-03/solution/list.h
+++ assignment-03/solution/list.h
@@ -2,11 +2,15 @@
 #define CPPPC__A03__LIST_H__INCLUDED
 
 #include <iterator>
+#include <iostream>
 
 namespace cpppc {
 
-  // list<int32_t, -1>
   template <typename ValueT, ValueT default_value = ValueT()>
+  class list;
+
+  // list<int32_t, -1>
+  template <typename ValueT, ValueT default_value>
  //   ValueT   default_value = ValueT()>
   class list {
     typedef list<ValueT, default_value> self_t;
@@ -18,16 +22,15 @@ namespace cpppc {
     };
     // list<T>::iterator
     class iterator {
-	//TODO I get an "expected nested-name-specifier" error if I use typename.. why?
-       //typedef typename list<ValueT, default_value> list_t;
        typedef list<ValueT, default_value> list_t;
        typedef typename list_t::list_node list_node_t;
        typedef typename list_t::iterator self_t;
+
     public:
        iterator() = delete;
 
-       iterator(const list_t & list)
-       : _list(list)
+       iterator(list_node & _node)
+       : _list_node(&_node)
        { }
 
        // const my_container & c;
@@ -53,63 +56,70 @@ namespace cpppc {
          return _list_node->value;
        }
 
-       bool operator==(const self_t & rhs) {
+       bool operator==(const iterator rhs) const
+       {
 	   return (this == &rhs || _list_node == rhs._list_node);
        }
 
-       bool operator!=(const self_t & rhs)
+       bool operator!=(const iterator rhs) const
        {
          return  !(*this == rhs);
        }
 
     private:
-       list_node_t * _list_node;
-       //TODO: you didn't put list in here, but you forward-declare it in line 23. Am I right in thinking it needs to go here, too? I get a compiler error here--> binding 'const list_t' to reference of type 'list_t::iterator::list_t&' discards qualifiers
-       list_t & _list;
+       list_node * _list_node;
+
     };
 
     
   public:
     list()
 	//here we define _begin and _end as iterators of this list object
-    : _begin(iterator(*this))
-    , _end(iterator(*this))
+    : _begin(iterator((*this)._head))
+    , _end(iterator((*this)._tail))
     { }
 
     // list<uint32_t>(list<int32_t>()) says 'nouh'
     list(const self_t & other)             = default;
     self_t & operator=(const self_t & rhs) = default;
 
-    bool operator==(const self_t & other)
+    bool operator==(const self_t & other) const
     {
-//	if (this == &other) //identity
-//	    return true;
-//	iterator i1 = *this.begin();
-//	iterator i2 = other.begin();
-//	TODO: Am I supposed to use _begin and _end to iterate? I would have to reset them at the end though wouldn't I?
-//
-//	while (i1 != *this.end() || i2 != other.end())
-//	{
-//	    if (*i1 != *i2)
-//		return false;
-//	    ++i1;
-//	    ++i2;
-//	}
-//
-//	return !(i1 == *this.end() && i2 == other.end());
+	if (this == &other) //identity
+	    return true;
 
+	else if ((*this).size() != other.size())
+	    return false;
+
+	else if ((*this).size() == 0) //hacky
+	    return true;
+
+	iterator ithis = begin();
+	iterator iother  = other.begin();
+
+	while (ithis != (*this).end() && iother != other.end())
+	{
+	    std::cout << "marker 1\n";
+	    if (*ithis != *iother)
+		return false;
+	    ++ithis;
+	    ++iother;
+	    std::cout << "marker 2\n";
+	}
+
+	return true; 
     }
 
     // list<int> l;
     // l_begin = l.begin();
     // ++l_begin;
-    iterator begin()  
+    iterator begin() const 
     { 
 	return _begin; 
     }
     // list<T> l;
     // *l.end();
-    iterator end()    
+    iterator end() const 
     { 
 	return _end; 
     }
@@ -131,55 +141,68 @@ namespace cpppc {
 	return (_size == 0);
     }
 
-    const list_node & front()
+    const ValueT & front()
     {
-	return _head;
+	return _head._value;
     }
 
     const list_node & back()
     {
-	return _tail;
+	return _tail.value;
     }
 
-    //here there be dragons
+    //TODO: Ich muss ja auf das next-Feld des ehemaligen letzten Elements zugreifen, um es auf das neue letzte zu setzen. Dazu muss ich auf das letzte Element der Liste zugreifen können. Dafür ist wahrscheinlich _tail gedacht. Allerdings verwende ich _tail (bzw es war ja von dir ursprünglich auch als static definiert) als leeres abschließendes Element. Brauche ich ein bestimmtes Feld, dass den "Inhalt" dieses letzten Elements deklariert, oder kann ich das direkt in den Konstruktor von _end() einfügen? (Zeile 78ff). Eigentlich brauche ich ja schon ein bestimmtes Member-Element, dass das abschließende ist.
     void push_back(const ValueT value)
     {
-//	list_node new_node = new list_node;
-//	back().next = &new_node;
-//	_tail = new_node;
-//	new_node.value = value;
-//	new_node.next = end();	
-//	++_size;
+	list_node new_node;
+       	new_node.next = &_tail;
+	new_node.value = value;
+	iterator i = begin();
+	while (i != back())
+	{
+	    ++i;
+	}
+	_ = &new_node;
+	++_size;
     }
 
     ValueT pop_back()
     {
-//	ValueT val = back().value;
-//	iterator(*this) i = begin();
+//	if (size() == 0)
+//	    return default_value;
+//
+//	iterator i = begin();
 //	while (i != back())
 //	{
 //		++i;
 //	}
-//	list_node new_tail = *i;
-//	delete back();
+//	ValueT val = *i;
+//	delete _tail;
 //	_tail = new_tail;
 //	return val;
     }
 
-    void push_front(const ValueT value);
-    ValueT pop_front();
+    void push_front(const ValueT value)
+    {
+
+    }
+
+    ValueT pop_front()
+    {
+
+    }
 
 //    iterator insert(iterator pos, const ValueT value);
     
   private:
     // same as = { }
-    list_node _head        = { nullptr, default_value };
-    static constexpr list_node _tail = { nullptr, default_value };
+    list_node _head = { nullptr, default_value };
+    list_node _tail = { nullptr, default_value };
     size_t _size = 0;
 
     // self_t * this
 
-    iterator  _begin = *this;
+    iterator  _begin; // = *this;
     iterator  _end;
   };
 
